797429305fa158f8e5dd4e74fcfc41a975e34ea9
VladVoynalovych <vlad.voynalovich@gmail.com>
2023-01-31 17:17:13 +0200

    update: font, movement bug fix, info panel markup and styles

--- src/Components/GameWrapper/GameWrapper.tsx
+++ src/Components/GameWrapper/GameWrapper.tsx
@@ -1,10 +1,113 @@
 import './index.css';
 import { Playground } from '../Playground/Playground';
+import { InfoPanel } from '../InfoPanel/InfoPanel';
+import { useEffect, useState } from 'react';
+import { deleteFilledRows, getRandomFigure, setupFigure } from '../../Extensions/FigureGenerator';
+import {
+  checkCollision,
+  moveFigureDown,
+  moveFigureLeft,
+  moveFigureRight,
+  rotateFigure,
+} from '../../Controller/MoveController';
+
+const emptyPlayground = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
 export const GameWrapper = () => {
+  const [{ figure, playground }, setGameState] = useState(() => ({
+    figure: getRandomFigure(),
+    playground: emptyPlayground,
+  }));
+
+  const moveDown = () => {
+    setGameState(({ figure, playground }) => {
+      if (!checkCollision(playground, moveFigureDown(figure))) {
+        return { figure: moveFigureDown(figure), playground };
+      } else {
+        let updatedPlayground = setupFigure(playground, figure);
+        updatedPlayground = deleteFilledRows(updatedPlayground);
+        return { figure: getRandomFigure(), playground: updatedPlayground };
+      }
+    });
+  };
+
+  useEffect(() => {
+    const gameLoopId = setInterval(moveDown, 500);
+    return () => {
+      clearInterval(gameLoopId);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleKeyPress = (e: KeyboardEvent) => {
+      switch (e.code) {
+        case 'ArrowDown':
+          moveDown();
+          break;
+        case 'ArrowRight':
+          setGameState((gameState) => {
+            if (!checkCollision(gameState.playground, moveFigureRight(gameState.figure))) {
+              return { ...gameState, figure: moveFigureRight(gameState.figure) };
+            }
+            return gameState;
+          });
+
+          break;
+        case 'ArrowLeft':
+          setGameState((gameState) => {
+            if (!checkCollision(gameState.playground, moveFigureLeft(gameState.figure))) {
+              return { ...gameState, figure: moveFigureLeft(gameState.figure) };
+            }
+            return gameState;
+          });
+          break;
+        case 'ArrowUp':
+          setGameState((gameState) => {
+            if (!checkCollision(gameState.playground, rotateFigure(gameState.figure))) {
+              return { ...gameState, figure: rotateFigure(gameState.figure) };
+            }
+            return gameState;
+          });
+          break;
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
   return (
     <div className='game-wrapper'>
       <h1>TETRIS</h1>
-      <Playground />
+
+      <div className='wrapper'>
+        <Playground playfield={playground} figure={figure} />
+        <InfoPanel />
+      </div>
     </div>
   );
 };
--- src/Components/InfoPanel/InfoPanel.tsx
+++ src/Components/InfoPanel/InfoPanel.tsx
@@ -1,1 +1,23 @@
-export {};
+import './index.css';
+
+export const InfoPanel = () => {
+  return (
+    <div className='info-panel'>
+      <div className='score-wrapper'>
+        <span className='score'>Score: 10</span>
+      </div>
+
+      <div className='rows-deleted-wrapper'>
+        <span className='rows-deleted'>Rows Deleted: 10</span>
+      </div>
+
+      <div className='level-wrapper'>
+        <span className='level'>Level: 1</span>
+      </div>
+
+      <div className='next-figure-wrapper'>
+        <span className='level'>Next figure</span>
+      </div>
+    </div>
+  );
+};
