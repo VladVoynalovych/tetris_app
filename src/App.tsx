822784f08beac302642cf52e9c9ec9f26d9c3609
VladVoynalovych <74152369+VladVoynalovych@users.noreply.github.com>
2023-02-02 15:44:04 +0000

    Deploying to gh-pages from @ VladVoynalovych/tetris_app@82ad904b5bf651000eb6737a522f5147b58f6030 🚀

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './App.css';
-import { GameWrapper } from './Components/GameWrapper/GameWrapper';
-
-function App() {
-  return <GameWrapper></GameWrapper>;
-}
-
-export default App;
